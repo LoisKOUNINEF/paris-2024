6f4de7ff1f47804f285185e67809f8ddec96358c
Lois KOUNINEF <lkouninef@gmail.com>
2025-04-03 00:15:21 +0200

    imported Auth, User, Guards  and Cron libraries in Server app

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';;
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { createDataSource } from '@paris-2024/server-data-source';
+import { AdminGuard, OwnerGuard, StaffGuard } from '@paris-2024/server-business-logic-guards';
+import { BusinessLogicCronModule } from '@paris-2024/server-business-logic-cron';
+import { PresentationAuthModule } from '@paris-2024/server-presentation-auth';
+import { PresentationUserModule } from '@paris-2024/server-presentation-user';
 
 @Module({
   imports: [
@@ -18,10 +23,25 @@ import { createDataSource } from '@paris-2024/server-data-source';
       inject: [ConfigService],
     }),
     ScheduleModule.forRoot(),
+    BusinessLogicCronModule,
+    PresentationAuthModule,
+    PresentationUserModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AdminGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: OwnerGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: StaffGuard,
+    },
   ],
 })
 export class AppModule {}
