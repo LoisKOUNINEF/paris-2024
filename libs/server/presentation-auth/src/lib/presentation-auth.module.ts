17015ce5cd72a651bc30fd703a3a5d5302dbfb92
Loïs Kouninef <97851048+LoisKOUNINEF@users.noreply.github.com>
2025-04-23 14:15:36 +0200

    fixed conflicts
    * fixed conflicts (2) - re-added line deleted by mistake
    
    * fixed conflicts (2) - re-added line deleted by mistake

--- libs/server/presentation-auth/src/lib/presentation-auth.module.ts
+++ libs/server/presentation-auth/src/lib/presentation-auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common";
 import { BusinessLogicAuthModule } from '@paris-2024/server-business-logic-auth';
 import { BusinessLogicUserModule } from "@paris-2024/server-business-logic-user";
 import { AuthController } from "./auth.controller";
-import { BusinessLogicCartModule } from "@paris-2024/server-presentation-auth";
+import { BusinessLogicCartModule } from "@paris-2024/server-business-logic-cart";
 
 @Module({
 	imports: [
