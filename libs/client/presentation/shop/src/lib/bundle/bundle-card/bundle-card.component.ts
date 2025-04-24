5acc897af57edb4c5c2eb2eeb5e6d29f0b6ac325
Lois KOUNINEF <lkouninef@gmail.com>
2025-04-24 17:16:46 +0200

    added styled bundle page

--- libs/client/presentation/shop/src/lib/bundle/bundle-card/bundle-card.component.ts
+++ libs/client/presentation/shop/src/lib/bundle/bundle-card/bundle-card.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { FormatPricePipe } from '@paris-2024/client-utils';
 import { Bundle } from '@paris-2024/client-data-access-bundle';
 
 @Component({
   selector: 'lib-bundle-card',
   standalone: true,
-  imports: [CurrencyPipe, FormatPricePipe],
+  imports: [CurrencyPipe, FormatPricePipe, CommonModule],
   templateUrl: './bundle-card.component.html',
   styleUrl: './bundle-card.component.scss',
 })
 export class BundleCardComponent {
+  imageUrl = 'assets/images/medal.avif';
+  imageAlt = 'Une sportive souriante avec sa médaile.';
+
   @Input({ required: true }) bundle: Bundle;
 }
