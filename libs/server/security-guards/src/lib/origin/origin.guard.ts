073312eeb0bdd98fa058fca14c20360e484ee6b7
Lois KOUNINEF <lkouninef@gmail.com>
2025-04-27 20:45:31 +0200

    fixed invalid origin when prefixed with "www."

--- libs/server/security-guards/src/lib/origin/origin.guard.ts
+++ libs/server/security-guards/src/lib/origin/origin.guard.ts
@@ -6,13 +6,19 @@ export class OriginGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<Request>();
     const origin = request.headers.origin || request.headers.referer;
+    const normalizeOrigin = (origin: string) => {
+      if (!origin) return '';
+      return origin.replace('https://www.', 'https://');
+    };
+
+    const allowedOrigins = [
+      'https://studi-exam-jo.lois-kouninef.eu',
+      'https://studi-exam-jo-staging.lois-kouninef.eu'
+    ];
 
     if (
-      request.hostname === 'localhost' || 
-      (origin && (
-        origin.startsWith('https://studi-exam-jo.lois-kouninef.eu') || 
-        origin.startsWith('https://studi-exam-jo-staging.lois-kouninef.eu')
-      ))
+      request.hostname === 'localhost' ||
+      (origin && allowedOrigins.some(allowed => normalizeOrigin(origin).startsWith(allowed)))
     ) {
       return true;
     }
