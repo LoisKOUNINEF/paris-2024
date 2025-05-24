a81f73b6847a11e17ba6a9a9449f23f7147116d1
Loïs Kouninef <97851048+LoisKOUNINEF@users.noreply.github.com>
2025-05-24 07:04:15 +0200

    Improvements (#44)
    * CI/CD: Bump version to 1.0.0
    
    * fixed ticket validity request
    
    * layout improvements
    
    * clearer client libs folder structure
    
    * clearer server libs folder structure
    
    * fixed migrations path (test)

---
+++ libs/server/ticket/data-access/src/lib/ticket.repository.ts
@@ -0,0 +1,75 @@
+import { Repository } from "typeorm";
+import { Ticket } from "./ticket.entity";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { QrCodeTicketDto } from "./ticket.dto";
+import { ticketDoesntExist } from "./ticket.exceptions";
+import { TicketValidity } from "@paris-2024/shared-interfaces";
+
+@Injectable()
+export class TicketRepository {
+  constructor(
+    @InjectRepository(Ticket) 
+    private ticketRepository: Repository<Ticket>,
+  ) {}
+
+  async create(dto: QrCodeTicketDto): Promise<Ticket> {
+    const ticket = this.ticketRepository.create(dto)
+    return await this.ticketRepository.save(ticket);
+  }
+
+  async findOneById(id: Ticket['id']): Promise<Ticket | undefined> {
+    const ticket = await this.ticketRepository.findOne({
+      where: { id: id }
+    })
+    if(!ticket) {
+      ticketDoesntExist();
+      return;
+    }
+    return ticket;
+  }
+
+  async findAllFromUser(userId: string): Promise<Array<Ticket>> {
+    return await this.ticketRepository
+      .createQueryBuilder('junction')
+      .leftJoinAndSelect('user', 'user', 'junction.user_id = user.id')
+      .where(`junction.user_id = :id::uuid`, { id: userId })
+      .getMany();
+  }
+
+  async findValids() {
+    return this.ticketRepository.find({
+      where: { isValid: true }
+    })
+  }
+
+  async isValid(userSecret: string, tokenHash: string): Promise<TicketValidity | null> {
+    const result = await this.ticketRepository
+      .createQueryBuilder('ticket')
+      .leftJoin('user', 'user', 'ticket.user_id::uuid = user.id')
+      .select([
+        'ticket.is_valid AS "isValid"',
+        'ticket.created_at AS "createdAt"',
+        'user.first_name AS "firstName"',
+        'user.last_name AS "lastName"'
+      ])
+      .where('ticket.token_hash = :tokenHash', { tokenHash })
+      .andWhere('user.secret_key = :userSecret', { userSecret })
+      .getRawOne();
+
+    if (!result) {
+      ticketDoesntExist();
+      return null;
+    }
+    
+    return {
+      isValid: result.isValid,
+      userFullName: `${result.firstName} ${result.lastName}`,
+      createdAt: result.createdAt,
+    };
+  }
+
+  async findAll(): Promise<Array<Ticket>> {
+    return await this.ticketRepository.find();
+  }
+}
